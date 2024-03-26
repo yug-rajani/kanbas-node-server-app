440d812b6e524c8c199be6ea2523404bd6e17bee
Yug Rajani <yug.rajani99@gmail.com>
2024-03-26 19:00:36 -0400

    Complete 3.2 - Working with Objects

--- Lab5.js
+++ Lab5.js
@@ -1,3 +1,19 @@
+const assignment = {
+    id: 1,
+    title: "NodeJS Assignment",
+    description: "Create a NodeJS server with ExpressJS",
+    due: "2021-10-10",
+    completed: false,
+    score: 0,
+};
+
+const module = {
+    id: "M01",
+    name: "NodeJS Module",
+    description: "NodeJS with ExpressJS",
+    course: "CS123",
+};
+
 const Lab5 = (app) => {
     app.get("/a5/welcome", (req, res) => {
         res.send("Welcome to Assignment 5");
@@ -48,5 +64,53 @@ const Lab5 = (app) => {
         }
         res.send(result.toString());
     });
+
+    app.get("/a5/assignment", (req, res) => {
+        res.json(assignment);
+    });
+
+    app.get("/a5/assignment/title", (req, res) => {
+        res.json(assignment.title);
+    });
+
+    app.get("/a5/assignment/title/:newTitle", (req, res) => {
+        const { newTitle } = req.params;
+        assignment.title = newTitle;
+        res.json(assignment);
+    });
+
+    app.get("/a5/assignment/score/:newScore", (req, res) => {
+        const { newScore } = req.params;
+        assignment.score = newScore;
+        res.json(assignment);
+    });
+
+    app.get("/a5/assignment/completed/:status", (req, res) => {
+        const { status } = req.params;
+        assignment.completed = status === "true";
+        res.json(assignment);
+    });
+
+    app.get("/a5/module", (req, res) => {
+        res.json(module);
+    });
+
+    app.get("/a5/module/name", (req, res) => {
+        res.json(module.name);
+    });
+
+    app.get("/a5/module/name/:newName", (req, res) => {
+        const { newName } = req.params;
+        module.name = newName;
+        res.json(module);
+    });
+
+    app.get("/a5/module/description/:newDescription", (req, res) => {
+        const { newDescription } = req.params;
+        module.description = newDescription;
+        res.json(module);
+    });
+
+
 };
 export default Lab5;
