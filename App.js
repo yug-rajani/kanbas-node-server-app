0cc151d4670cb53e0c5ea5838449ed3b204b45fd
Yug Rajani <yug.rajani99@gmail.com>
2024-03-28 20:43:45 -0400

    Implementing Routes for Courses

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
-import cors from 'cors';
+import CourseRoutes from "./Kanbas/courses/routes.js";
 
 const app = express();
 app.use(cors());
+CourseRoutes(app);
 app.use(express.json());
 Hello(app);
 Lab5(app);
---
+++ Kanbas/Database/index.js
@@ -0,0 +1,9 @@
+import courses from "./courses.js";
+import modules from "./modules.js";
+import events from "./events.js";
+import assignments from "./assignments.js";
+import users from "./users.js";
+import grades from "./grades.js";
+import enrollments from "./enrollments.js";
+
+export default { courses, modules, events, assignments, users, grades, enrollments };
