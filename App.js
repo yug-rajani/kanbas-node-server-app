5da5bc1b649deb454a1524f852690eb60a1e6090
Yug Rajani <yug.rajani99@gmail.com>
2024-03-29 18:20:30 -0400

    Complete till 4.3.4 - Modules

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import cors from 'cors';
 import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
 
 const app = express();
 app.use(cors());
-CourseRoutes(app);
 app.use(express.json());
+ModuleRoutes(app);
+CourseRoutes(app);
 Hello(app);
 Lab5(app);
 app.listen(4000);
