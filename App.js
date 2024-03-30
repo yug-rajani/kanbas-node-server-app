25fd11d1f1d87a4d684a1d0b798af90bad5176c2
Yug Rajani <yug.rajani99@gmail.com>
2024-03-30 01:07:36 -0400

    Complete 4.3: Assignments (grads only)

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import Hello from './Hello.js';
 import Lab5 from './Lab5.js';
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+AssignmentRoutes(app);
 Hello(app);
 Lab5(app);
 app.listen(4000);
--- Kanbas/Database/assignments.js
+++ Kanbas/Database/assignments.js
@@ -3,45 +3,90 @@ export default [
         _id: "6557fcb7a11aca237708374b",
         title: "Propulsion Assignment",
         course: "RS101",
+        points: 100,
+        due_date: "Oct 2 2022",
+        due_time: "9:00 AM",
+        week_description: "Week 4: Advanced Propulsion Systems - Starting on September 26th",
+        description: "Assignment on advanced propulsion systems"
     },
     {
         _id: "6557fcb7a11aca237708374c",
         title: "Combustion Analysis",
         course: "RS101",
+        points: 100,
+        due_date: "Oct 2 2022",
+        due_time: "9:00 AM",
+        week_description: "Week 4: Advanced Propulsion Systems - Starting on September 26th",
+        description: "Assignment on advanced propulsion systems"
     },
     {
         _id: "6557fcb7a11aca237708374d",
         title: "Nozzle Design Project",
         course: "RS101",
+        points: 100,
+        due_date: "Oct 2 2022",
+        due_time: "9:00 AM",
+        week_description: "Week 4: Advanced Propulsion Systems - Starting on September 26th",
+        description: "Assignment on advanced propulsion systems"
     },
     {
         _id: "6557fcb7a11aca237708374e",
         title: "Aerodynamics Quiz",
         course: "RS102",
+        points: 100,
+        due_date: "Oct 2 2022",
+        due_time: "9:00 AM",
+        week_description: "Week 4: Advanced Propulsion Systems - Starting on September 26th",
+        description: "Assignment on advanced propulsion systems"
     },
     {
         _id: "6557fcb7a11aca237708374f",
         title: "Flow Analysis",
         course: "RS102",
+        points: 100,
+        due_date: "Oct 2 2022",
+        due_time: "9:00 AM",
+        week_description: "Week 4: Advanced Propulsion Systems - Starting on September 26th",
+        description: "Assignment on advanced propulsion systems"
     },
     {
         _id: "6557fcb7a11aca2377083750",
         title: "Heating Analysis",
         course: "RS102",
+        points: 100,
+        due_date: "Oct 2 2022",
+        due_time: "9:00 AM",
+        week_description: "Week 4: Advanced Propulsion Systems - Starting on September 26th",
+        description: "Assignment on advanced propulsion systems"
     },
     {
         _id: "6557fcb7a11aca2377083751",
         title: "Structural Design Task",
         course: "RS103",
+        points: 100,
+        due_date: "Oct 2 2022",
+        due_time: "9:00 AM",
+        week_description: "Week 4: Advanced Propulsion Systems - Starting on September 26th",
+        description: "Assignment on advanced propulsion systems"
     },
     {
         _id: "6557fcb7a11aca2377083752",
         title: "Orbital Calculations",
         course: "RS103",
+        points: 100,
+        due_date: "Oct 2 2022",
+        due_time: "9:00 AM",
+        week_description: "Week 4: Advanced Propulsion Systems - Starting on September 26th",
+        description: "Assignment on advanced propulsion systems"
     },
     {
         _id: "6557fcb7a11aca2377083753",
         title: "Systems Engineering Exam",
         course: "RS103",
+        points: 100,
+        due_date: "Oct 2 2022",
+        due_time: "9:00 AM",
+        week_description: "Week 4: Advanced Propulsion Systems - Starting on September 26th",
+        description: "Assignment on advanced propulsion systems"
     },
 ];
