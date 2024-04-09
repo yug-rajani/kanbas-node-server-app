f0252bf40853de0c3ba95e03d775cd222b5c454c
Yug Rajani <yug.rajani99@gmail.com>
2024-04-09 11:22:51 -0400

    Complete till 3.5.4

--- Users/routes.js
+++ Users/routes.js
@@ -2,25 +2,37 @@ import * as dao from "./dao.js";
 let currentUser = null;
 export default function UserRoutes(app) {
     const createUser = async (req, res) => { };
+
     const deleteUser = async (req, res) => { };
-    const findAllUsers = async (req, res) => { };
+
+    const findAllUsers = async (req, res) => {
+        const users = await dao.findAllUsers();
+        res.json(users);
+    };
+
     const findUserById = async (req, res) => { };
+
     const updateUser = async (req, res) => {
         const { userId } = req.params;
         const status = await dao.updateUser(userId, req.body);
         currentUser = await dao.findUserById(userId);
         res.json(status);
     };
+
     const signup = async (req, res) => { };
+
     const signin = async (req, res) => {
         const { username, password } = req.body;
         currentUser = await dao.findUserByCredentials(username, password);
         res.json(currentUser);
     };
+
     const signout = (req, res) => { };
+
     const profile = async (req, res) => {
         res.json(currentUser);
     };
+
     app.post("/api/users", createUser);
     app.get("/api/users", findAllUsers);
     app.get("/api/users/:userId", findUserById);
