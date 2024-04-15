2526dcbdc6b662780f490425324f1bd50ceb9c42
Yug Rajani <yug.rajani99@gmail.com>
2024-04-15 10:03:42 -0400

    Complete 4.2

--- Users/routes.js
+++ Users/routes.js
@@ -53,7 +53,10 @@ export default function UserRoutes(app) {
         res.json(currentUser);
     };
 
-    const signout = (req, res) => { };
+    const signout = (req, res) => {
+        currentUser = null;
+        res.sendStatus(200);
+    };
 
     const profile = async (req, res) => {
         res.json(currentUser);
