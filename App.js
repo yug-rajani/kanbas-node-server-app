1d9302e717263ef6c39ab5f892c43160c82be926
Yug Rajani <yug.rajani99@gmail.com>
2024-04-15 22:29:21 -0400

    Change connection string

--- App.js
+++ App.js
@@ -10,9 +10,9 @@ import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 import mongoose from "mongoose";
 import UserRoutes from "./Users/routes.js";
 
-// const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
-// mongoose.connect(CONNECTION_STRING);
-mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING);
+// mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 
 // Test the connection
 const db = mongoose.connection;
